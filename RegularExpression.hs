5c99bba5baa13e2899ce42c357b0917f47842d42
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2014-01-12 11:12:18 +0530

    Refactor code

--- RegularExpression.hs
+++ RegularExpression.hs
@@ -15,19 +15,16 @@ data Reg = Epsilon
          deriving ( Show, Eq )
 
 exprCal = buildExpressionParser table atom where 
-   table = [ [ Postfix (  Star <$ char '*' ) ]
-            ,[ Infix ( return  sequence) AssocLeft ]
-            ,[ Infix ( choice <$ char '|' ) AssocLeft ]
+   table = [ [ Postfix ( Star <$ char '*' )           ]
+            ,[ Infix   ( return  sequence ) AssocLeft  ]
+            ,[ Infix   ( Or   <$ char '|' ) AssocLeft ]
            ]
 
    
    lit = noneOf "*|()"
    sequence a b = Then  (seqTerms a)  (seqTerms b)
-   choice a b = Or  (choiceTerms a)  (choiceTerms b)
    seqTerms (Then ts ps ) = Then ( seqTerms ts ) ( seqTerms ps )
    seqTerms t = t
-   choiceTerms (Or ts ps ) = Or ( choiceTerms ts ) ( choiceTerms ps )
-   choiceTerms t =  t
    atom =  char '(' *> exprCal <* char ')'
        <|> ( Literal <$> lit )
        
