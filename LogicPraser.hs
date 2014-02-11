b989f7be30de51b09ddbad08d8cdd2f57a8c7e40
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2014-02-11 20:32:38 +0530

    Added evaluator to parser

--- LogicPraser.hs
+++ LogicPraser.hs
@@ -1,62 +1,51 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
-import Text.Parsec.Token
 import Text.Parsec.Prim
-import Text.Parsec.Char
 import Text.Parsec.Expr
-import Text.Parsec.Combinator
-import Text.Parsec.Language
+import Text.Parsec.Char
+import Text.Parsec.String ( Parser )
 import Control.Applicative hiding ( ( <|> ) , many )
-import Data.Maybe ( fromJust )
 
 
 
-data LExpr = Lit Char
-           | Not LExpr
+data LExpr = Lit Bool
+           | Not LExpr       
            | And LExpr LExpr
            | Or LExpr LExpr
-           | Imp LExpr LExpr  -- (=>)
-           | Red LExpr LExpr  -- ( <= )
-           | Eqi LExpr LExpr  -- ( <=> ) 
+           | Imp LExpr LExpr  -- ( P =>  Q )
+           | Red LExpr LExpr  -- ( P <=  Q )
+           | Eqi LExpr LExpr  -- ( P <=> Q ) 
            deriving Show
 
+exprCal :: Parser LExpr
 exprCal = buildExpressionParser table atom 
 
-table = [  [ Prefix ( Not <$ string  "~"  ) ]
-         , [ Infix  ( And <$ string  "&"  ) AssocLeft ]
-         , [ Infix  ( Or  <$ string "|"   ) AssocLeft ]
+table = [  [  Prefix ( Not <$ string  "~"  ) ]
+         , [  Infix  ( And <$ string  "&"  ) AssocLeft ]
+         , [  Infix  ( Or  <$ string "|"   ) AssocLeft ]
          , [  Infix  ( Eqi <$ try ( string "<=>" ) ) AssocLeft 
             , Infix  ( Imp <$  string "=>"         ) AssocLeft 
             , Infix  ( Red <$  string "<="         ) AssocLeft 
            ]
         ]
 
-
+atom :: Parser LExpr
 atom =  char '(' *>  exprCal   <* char ')' 
-     <|> ( Lit <$> letter )
+     <|> ( Lit <$> ( ( True  <$ string "True"  )  <|> ( False <$ string "False" ) ) )
 
 
 
 expEval :: LExpr -> Bool
-expEval ( Lit 't' ) = True
-expEval ( Lit 'f' ) = False
-expEval ( Not ( Lit 't' ) ) = False
-expEval ( Not ( Lit 'f' ) ) = True
-expEval ( Not a ) = not  ( expEval  a )
-expEval ( And ( Lit 't' ) ( Lit 't' ) ) = True
-expEval ( And ( Lit _ )  ( Lit _ ) ) = False
-expEval ( And a b ) = ( && ) ( expEval a ) ( expEval b )
-expEval ( Or ( Lit 'f' ) ( Lit 'f' ) ) = False
-expEval ( Or ( Lit _ ) ( Lit _ ) ) = True
-expEval ( Or a b ) = ( || ) ( expEval a ) ( expEval b )
-expEval ( Imp ( Lit 'f' ) ( Lit _ ) ) = True
-expEval ( Imp ( Lit 't' ) ( Lit 't' ) ) = True
-expEval ( Imp ( Lit 't' ) ( Lit 'f' ) ) = False
-expEval ( Imp a b ) = expEval ( Imp ( expEval a ) ( expEval b ) )
-expEval ( Red p q ) = expEval ( Imp q p ) -- just reverse the arguments. 
-expEval ( Eqi ( Lit 't' ) ( Lit 't' ) ) = True
-expEval ( Eqi ( Lit 'f' ) ( Lit 'f' ) ) = True
-expEval ( Eqi ( Lit _ ) ( Lit _ ) ) = False
-expEval ( Eqi a b ) = expEval ( Eqi ( expEval a ) ( expEval b ) )
+expEval ( Lit True ) = True
+expEval ( Lit False ) = False
+expEval ( Not expr ) = not . expEval $ expr
+expEval ( And exprF exprS ) = ( && ) (  expEval exprF ) ( expEval  exprS )
+expEval ( Or exprF exprS ) = ( || ) ( expEval exprF ) ( expEval exprS )
+expEval ( Imp exprF exprS ) = ( || ) ( not . expEval $ exprF ) ( expEval exprS )
+expEval ( Red exprF exprS ) = expEval ( Imp exprS exprF ) 
+expEval ( Eqi exprF exprS ) 
+       | first == second = True
+       | otherwise = False where
+          first  = expEval exprF
+          second = expEval exprS 
 
 
 calculator :: String -> LExpr
