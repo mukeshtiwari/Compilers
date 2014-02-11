b0dd880bd204fb53a2ee0d098dc2c9d5c9989d0b
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2014-02-11 20:46:47 +0530

    Minor indentation

--- LogicPraser.hs
+++ LogicPraser.hs
@@ -34,21 +34,21 @@ atom =  char '(' *>  exprCal   <* char ')'
 
 
 expEval :: LExpr -> Bool
-expEval ( Lit True ) = True
-expEval ( Lit False ) = False
-expEval ( Not expr ) = not . expEval $ expr
-expEval ( And exprF exprS ) = ( && ) (  expEval exprF ) ( expEval  exprS )
-expEval ( Or exprF exprS ) = ( || ) ( expEval exprF ) ( expEval exprS )
-expEval ( Imp exprF exprS ) = ( || ) ( not . expEval $ exprF ) ( expEval exprS )
-expEval ( Red exprF exprS ) = expEval ( Imp exprS exprF ) 
-expEval ( Eqi exprF exprS ) 
-       | first == second = True
-       | otherwise = False where
-          first  = expEval exprF
-          second = expEval exprS 
+expEval ( Lit True  )       = True
+expEval ( Lit False )       = False
+expEval ( Not expr  )       = not . expEval $ expr
+expEval ( And exprf exprs ) = ( && ) (  expEval exprf ) ( expEval  exprs )
+expEval ( Or  exprf exprs ) = ( || ) (  expEval exprf ) ( expEval  exprs )
+expEval ( Imp exprf exprs ) = ( || ) (  not . expEval $ exprf ) ( expEval exprs )
+expEval ( Red exprf exprs ) = expEval ( Imp exprs exprf ) 
+expEval ( Eqi exprf exprs ) 
+       | first == second  = True
+       | otherwise  = False where
+          first  = expEval exprf
+          second = expEval exprs 
 
 
-calculator :: String -> LExpr
+calculator :: String -> Bool
 calculator expr = case parse  exprCal ""  expr of
                        Left msg -> error "failed to parse"
-                       Right ( val ) -> val
+                       Right val -> expEval val
