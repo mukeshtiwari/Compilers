1d65336f4bd4fb83ed5329bc6718bab41144d474
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2014-04-02 02:58:28 +0530

    Added interpreter

--- LogicPraser.hs
+++ LogicPraser.hs
@@ -3,10 +3,12 @@ import Text.Parsec.Expr
 import Text.Parsec.Char
 import Text.Parsec.String ( Parser )
 import Control.Applicative hiding ( ( <|> ) , many )
+import qualified Data.Map as M
+import Data.Maybe
+import Data.List
+import Control.Monad
 
-
-
-data LExpr = Lit Bool
+data LExpr = Lit Char
            | Not LExpr       
            | And LExpr LExpr
            | Or LExpr LExpr
@@ -20,7 +22,7 @@ exprCal = buildExpressionParser table atom
 
 table = [  [  Prefix ( Not <$ string  "~"  ) ]
          , [  Infix  ( And <$ string  "&"  ) AssocLeft ]
-         , [  Infix  ( Or  <$ string "|"   ) AssocLeft ]
+         , [  Infix  ( Or  <$ string  "|"  ) AssocLeft ]
          , [  Infix  ( Eqi <$ try ( string "<=>" ) ) AssocLeft 
             , Infix  ( Imp <$  string "=>"         ) AssocLeft 
             , Infix  ( Red <$  string "<="         ) AssocLeft 
@@ -29,26 +31,51 @@ table = [  [  Prefix ( Not <$ string  "~"  ) ]
 
 atom :: Parser LExpr
 atom =  char '(' *>  exprCal   <* char ')' 
-     <|> ( Lit <$> ( ( True  <$ string "True"  )  <|> ( False <$ string "False" ) ) )
+     <|> ( Lit <$> letter )
+
+assignment :: LExpr -> [ M.Map Char Bool ]
+assignment expr = map ( M.fromList . zip vs ) ps where
+    vs = variables expr
+    ps = replicateM ( length vs ) [ True, False]
+ 
+
+variables :: LExpr -> [ Char ]
+variables expr = map head . group . sort . vars expr $ []  where
+    vars ( Lit c )    xs = c : xs
+    vars ( Not expr ) xs = vars expr xs
+    vars ( And exprf exprs ) xs = vars exprf xs ++ vars exprs xs
+    vars ( Or  exprf exprs ) xs = vars exprf xs ++ vars exprs xs
+    vars ( Imp exprf exprs ) xs = vars exprf xs ++ vars exprs xs
+    vars ( Red exprf exprs ) xs = vars exprf xs ++ vars exprs xs
+    vars ( Eqi exprf exprs ) xs = vars exprf xs ++ vars exprs xs
 
 
-
-expEval :: LExpr -> Bool
-expEval ( Lit True  )       = True
-expEval ( Lit False )       = False
-expEval ( Not expr  )       = not . expEval $ expr
-expEval ( And exprf exprs ) = ( && ) (  expEval exprf ) ( expEval  exprs )
-expEval ( Or  exprf exprs ) = ( || ) (  expEval exprf ) ( expEval  exprs )
-expEval ( Imp exprf exprs ) = ( || ) (  not . expEval $ exprf ) ( expEval exprs )
-expEval ( Red exprf exprs ) = expEval ( Imp exprs exprf ) 
-expEval ( Eqi exprf exprs ) 
+expEval :: LExpr -> M.Map Char  Bool -> Bool
+expEval ( Lit v   )         mp =  fromMaybe False ( M.lookup v mp )
+expEval ( Not expr  )       mp =  not . expEval  expr $ mp
+expEval ( And exprf exprs ) mp =  expEval exprf mp &&   expEval  exprs mp
+expEval ( Or  exprf exprs ) mp =  expEval exprf mp ||   expEval  exprs mp
+expEval ( Imp exprf exprs ) mp =  (  not . expEval  exprf $ mp  ) ||   expEval exprs mp 
+expEval ( Red exprf exprs ) mp =  expEval ( Imp exprs exprf  ) mp 
+expEval ( Eqi exprf exprs ) mp
        | first == second  = True
        | otherwise  = False where
-          first  = expEval exprf
-          second = expEval exprs 
+          first  = expEval exprf mp
+          second = expEval exprs mp
 
+values :: LExpr -> [ Bool ]
+values expr = map ( expEval expr ) ( assignment expr ) 
 
-calculator :: String -> Bool
+isTautology :: LExpr -> Bool
+isTautology = and . values
+
+isContradiction :: LExpr -> Bool
+isContradiction  = not . or . values
+
+isContingent :: LExpr -> Bool
+isContingent expr = not (isTautology expr || isContradiction expr)
+  
+calculator :: String -> LExpr
 calculator expr = case parse  exprCal ""  expr of
                        Left msg -> error "failed to parse"
-                       Right val -> expEval val
+                       Right val -> val
